b6c6a0cfe65c845b18ee9ae788b67d5ac7a488c8
edyting <dankyiemmanuel0@gmail.com>
2024-10-08 18:23:03 +0000

    working with post request

--- server.js
+++ server.js
@@ -5,12 +5,21 @@ app.set("view engine","ejs");
 // middleware
 app.use(express.static("public"));
 
+// to get data from form
+app.use(express.urlencoded({extended:false}))
+
 app.get('/',(req,res)=>{
     res.render("homepage")
 });
 
 app.get('/login',(req,res)=>{
     res.render("login")
+});
+
+app.post('/register',(req,res)=>{
+    console.log(req.body);
+    res.send("Thank You")
+    
 });
 
 app.listen(5000,console.log("listening on port 5000..."))
